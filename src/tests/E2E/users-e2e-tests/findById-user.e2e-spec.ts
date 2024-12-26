c5885bc63a12bbd6502fc1aa37a8d69f4d02a8be
root <root@SecHex-437L3JC>
2024-12-26 18:33:00 -0300

    feat: create find user by id route tests

---
+++ src/tests/E2E/users-e2e-tests/findById-user.e2e-spec.ts
@@ -0,0 +1,86 @@
+import { ValidationPipe, type INestApplication } from '@nestjs/common';
+import { Test, type TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/infra/db/Prisma/prisma.service';
+
+describe('Find-UserById Tests (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(new PrismaService())
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    await app.init();
+
+    const prisma = app.get(PrismaService);
+
+    await prisma.user.create({
+      data: {
+        id: 1,
+        name: 'Jhon Doe',
+        email: 'jhondoe@example.com',
+        password: '%)#&%^&%$!@(',
+      },
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Should not found a user with a NaN id', async () => {
+    const response = await request(app.getHttpServer()).get('/user/Jhon');
+
+    console.log('Response =', response.body);
+    expect(response.status).toBe(400);
+
+    expect(response.body.message[0]).toBe('O valor deve ser maior que 0.');
+  });
+
+  it('Should not found a user with a not existent id', async () => {
+    const response = await request(app.getHttpServer()).get(
+      '/user/99999',
+    );
+
+    console.log('Response =', response.body);
+    expect(response.status).toBe(404);
+
+    expect(response.body.message).toBe('Cannot find user with id 99999');
+  });
+
+  it('Should found a user with an existing id', async () => {
+    const response = await request(app.getHttpServer()).get('/user/1');
+
+    console.log('Response =', response.body);
+    expect(response.status).toBe(200);
+
+    expect(response.body.userProps.name).toBe('Jhon Doe');
+    expect(response.body.userProps.email).toBe('jhondoe@example.com');
+    expect(response.body.userProps.password).toBe('%)#&%^&%$!@(');
+
+    expect(response.body.userProps.id).toBeDefined();
+    expect(typeof response.body.userProps.id).toBe('number');
+    expect(response.body.userProps.id).toBe(1);
+    expect(response.body.userProps.id).toBeGreaterThan(0);
+
+    expect(response.body.userProps.createdAt).toBeDefined();
+    expect(new Date(response.body.userProps.createdAt).toString()).not.toBe(
+      'Invalid Date',
+    );
+  });
+});
